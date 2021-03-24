cc0a43dfc49654211b47d8933ddffbac6328ed9f
DingLuBJUT <56061870+DingLuBJUT@users.noreply.github.com>
2021-03-24 17:15:35 +0800

    Update predict.py

---
+++ code/semantic_match/predict.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from tqdm.autonotebook import tqdm
+
+import torch
+from torch.utils.data import DataLoader
+
+from model import PretrainedBERT
+from dataset import BertDataset
+
+
+def predict(model_path, test_data, vocab, result_path):
+
+    batch_size = 64
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu:0'
+
+    embedding_dim = 768
+    max_len = 64
+    model = PretrainedBERT(embedding_size=len(vocab),
+                           embedding_dim=embedding_dim,
+                           max_len=max_len)
+    model.load_state_dict(torch.load(model_path)['model'])
+    model = model.to(device)
+
+    test_dataset = BertDataset(test_data.values, vocab, max_seq_len=64, data_type='test')
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+    result = []
+    for data in tqdm(test_loader):
+        data['input_ids'] = data['input_ids'].to(device)
+        data['token_type_ids'] = data['token_type_ids'].to(device)
+        data['attention_mask'] = data['attention_mask'].to(device)
+        predict_output = model(data)
+        predict_output = predict_output[:, 0, 5:7].cpu().detach().numpy()
+        predict_output = predict_output[:, 1] / (predict_output.sum(axis=1) + 1e-8)
+        result.append(predict_output)
+    result = np.concatenate(result)
+    result = pd.DataFrame(result, columns=['label'])
+    result['label'].to_csv(result_path, sep='\t', index=0, header=False)
+    return
