3a62d60350e8f49e96fb75c3a93a818ed0e33c05
DingLuBJUT <56061870+DingLuBJUT@users.noreply.github.com>
2021-03-28 19:42:43 +0800

    Update model.py

--- code/semantic_match/model.py
+++ code/semantic_match/model.py
@@ -1,11 +1,16 @@
-from torch.nn import Module, Linear, LayerNorm
+from torch.nn import Module, Linear, LayerNorm, Embedding, Parameter
 from transformers import BertModel
 
-
 class PretrainedBERT(Module):
-    def __init__(self, embedding_size, embedding_dim, max_len):
+    def __init__(self, embedding_size, embedding_dim, max_len, keep_index=None):
         super(PretrainedBERT, self).__init__()
         self.bert = BertModel.from_pretrained('bert-base-chinese')
+        if keep_index is not None:
+            self.embedding = Embedding(embedding_size,embedding_dim)
+            weight = self.bert.embeddings.word_embeddings.weight[keep_index]
+            self.embedding.weight = Parameter(weight)
+            self.bert.embeddings.word_embeddings = self.embedding
+
         self.lr1 = Linear(in_features=embedding_dim, out_features=embedding_dim)
         self.ln1 = LayerNorm([max_len, embedding_dim])
         self.lr2 = Linear(in_features=embedding_dim, out_features=embedding_size)
