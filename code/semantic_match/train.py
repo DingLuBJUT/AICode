9b3bea476d046f5e65f4b96677fc69fee3735328
DingLuBJUT <56061870+DingLuBJUT@users.noreply.github.com>
2021-03-28 19:47:27 +0800

    Update train.py

--- code/semantic_match/train.py
+++ code/semantic_match/train.py
@@ -1,53 +1,17 @@
-import os
-import numpy as np
-import pandas as pd
-from tqdm.autonotebook import tqdm
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
-
-import torch
-from torch.optim import Adam
-from torch.nn import CrossEntropyLoss
-from torch.utils.data import DataLoader
-
-from model import PretrainedBERT
-from dataset import BertDataset
-from utils import get_vocab_dict
-
-
-def evaluate(model, val_loader, device):
-    model.eval()
-    predicts = []
-    labels = []
-    for data, _ in val_loader:
-        data['input_ids'] = data['input_ids'].to(device)
-        data['token_label'] = data['token_label'].to(device)
-        data['token_type_ids'] = data['token_type_ids'].to(device)
-        data['attention_mask'] = data['attention_mask'].to(device)
-        output = model(data)
-        predict = output[:, 0, 5:7].cpu().detach().numpy()
-        predict = predict[:, 1] / (predict.sum(axis=1) + 1e-8)
-        predicts.append(predict)
-        labels.append(data['token_label'][:, 0].cpu().numpy() - 5)
-    labels = np.concatenate(labels)
-    predicts = np.concatenate(predicts)
-    auc_score = roc_auc_score(labels, predicts)
-    return auc_score
-
-
-def train(data, vocab):
+def train(data, vocab, keep_index):
     num_epochs = 100
     batch_size = 32
     early_stopping = 5
     learning_rate = 2e-5
-    save_model_dir = "/content/"
+    save_model_dir = "/content/gdrive/MyDrive/model/"
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu:0'
     max_len = 64
     embedding_dim = 768
 
     model = PretrainedBERT(embedding_size=len(vocab),
                            embedding_dim=embedding_dim,
-                           max_len=max_len)
+                           max_len=max_len,
+                           keep_index=keep_index)
     criterion = CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=learning_rate)
 
@@ -66,23 +30,24 @@ def train(data, vocab):
     best_auc_score = 0.0
     stopping_num = 0
     last_model_path = None
+
     for i, epoch in enumerate(range(num_epochs)):
         train_losses = []
         show_bar = tqdm(train_loader)
-        for data, _ in show_bar:
-            data['input_ids'] = data['input_ids'].to(device)
-            data['token_label'] = data['token_label'].to(device)
-            data['token_type_ids'] = data['token_type_ids'].to(device)
-            data['attention_mask'] = data['attention_mask'].to(device)
+        for input_data, _ in show_bar:
+            input_data['input_ids'] = input_data['input_ids'].to(device)
+            input_data['token_label'] = input_data['token_label'].to(device)
+            input_data['token_type_ids'] = input_data['token_type_ids'].to(device)
+            input_data['attention_mask'] = input_data['attention_mask'].to(device)
             optimizer.zero_grad()
-            output = model(data)
-            token_mask = (data['token_label'] != -1)
+            output = model(input_data)
+            token_mask = (input_data['token_label'] != -1)
             train_loss = criterion(output[token_mask].view(-1, len(vocab)),
-                                   data['token_label'][token_mask].view(-1))
+                                    input_data['token_label'][token_mask].view(-1))
             train_loss.backward()
             optimizer.step()
             train_losses.append(train_loss.cpu().detach().numpy())
-            show_bar.set_description("The Epoch is {0}, the Train Loss is {1:0.2f}"
+            show_bar.set_description("Epoch {0}, Loss {1:0.2f}"
                                      .format(epoch, np.mean(train_losses)))
 
         val_auc_score = evaluate(model, val_loader, device)
@@ -100,8 +65,3 @@ def train(data, vocab):
             if last_model_path is not None:
                 os.remove(last_model_path)
             last_model_path = model_path
-        else:
-            stopping_num += 1
-            if stopping_num >= early_stopping:
-                break
-    return
