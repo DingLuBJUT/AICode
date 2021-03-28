5ef5d6ae08e70734cc83e931972aa146f07eaa20
DingLuBJUT <56061870+DingLuBJUT@users.noreply.github.com>
2021-03-28 19:43:10 +0800

    Update utils.py

--- code/semantic_match/utils.py
+++ code/semantic_match/utils.py
@@ -1,3 +1,5 @@
+import json
+import numpy as np
 from collections import defaultdict
 
 def get_vocab_dict(data, list_special_tokens=None, min_count=5):
@@ -13,3 +15,19 @@ def get_vocab_dict(data, list_special_tokens=None, min_count=5):
     vocab = list_special_tokens + list(vocab.keys())
     vocab = dict(zip(vocab, range(len(vocab))))
     return vocab
+
+
+def get_keep_index(corpus_vocab_path, count_path, list_special_index, used_size):
+    count = json.load(open(count_path))
+    del count['[CLS]']
+    del count['[SEP]']
+
+    corpus_vocab = []
+    with open(corpus_vocab_path,'r') as f:
+        for line in f.readlines():
+            corpus_vocab.append(line.strip())
+
+    frequency = [count.get(token, 0) for token in corpus_vocab]
+    keep_index = list(np.argsort(frequency)[::-1])
+    keep_index = list_special_index + keep_index[:used_size]
+    return keep_index
